d8e9e5f967d87728d5b9367e30a6e7a2945fd808
Pr0b1em <947959992@qq.com>
2022-02-28 23:08:22 +0800

    Site updated: 2022-02-28 23:07:18

---
+++ js/src/bootstrap.js
@@ -0,0 +1,47 @@
+/* global NexT, CONFIG */
+
+$(document).ready(function() {
+
+  $(document).trigger('bootstrap:before');
+
+  /**
+   * Register JS handlers by condition option.
+   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
+   */
+  CONFIG.fastclick && NexT.utils.isMobile() && window.FastClick.attach(document.body);
+  CONFIG.lazyload && NexT.utils.lazyLoadPostsImages();
+
+  NexT.utils.registerESCKeyEvent();
+
+  NexT.utils.registerBackToTop();
+
+  // Mobile top menu bar.
+  $('.site-nav-toggle button').on('click', function() {
+    var $siteNav = $('.site-nav');
+    var ON_CLASS_NAME = 'site-nav-on';
+    var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
+    var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
+    var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
+
+    $siteNav.stop()[animateAction]('fast', function() {
+      $siteNav[animateCallback](ON_CLASS_NAME);
+    });
+  });
+
+  // Define Motion Sequence.
+  NexT.motion.integrator
+    .add(NexT.motion.middleWares.logo)
+    .add(NexT.motion.middleWares.sidebarInit)
+    .add(NexT.motion.middleWares.sidebar)
+    //Reset after pjax
+    .add(NexT.motion.middleWares.menu)
+    .add(NexT.motion.middleWares.reload)
+    .add(NexT.motion.middleWares.postList);
+
+  $(document).trigger('motion:before');
+
+  // Bootstrap Motion.
+  CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
+
+  $(document).trigger('bootstrap:after');
+});
---
+++ js/src/pjax.js
@@ -0,0 +1,35 @@
+document.addEventListener("pjax:send", function () {
+});
+
+document.addEventListener("pjax:complete", function () {
+  NexT.motion.integrator.reset(-1);
+  CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
+  if (window.DISQUS) {
+    try {
+      DISQUS.reset({reload: true, config: disqus_config});
+    } catch {
+    }
+  }
+  else {
+    loadComments();
+  }
+  if (window.DISQUSWIDGETS) {
+    DISQUSWIDGETS.getCount()
+  }
+});
+
+document.addEventListener("pjax:error", function () {
+  console.log("pjax:error");
+});
+
+document.addEventListener("pjax:success", function () {
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Pjax({
+    elements: ["a[href]:not(.fancybox)"],
+    selectors: [".container"],
+    cacheBust: false
+    // currentUrlFullReload: true,
+  });
+});
